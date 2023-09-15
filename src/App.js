0ea778be31ce8fe9ae541a71ad70241a44612dfa
SandeepKushwaha <sandeepkush327@gmail.com>
2023-09-15 21:35:06 +0530

    intial started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() { 
+    console.log('RENDER');
+    return (
+      <div className="App">
+        <h1>Component Life Cycle</h1>
+        {this.state.count}
+        <button onClick={this.handleClick} >Increment Count</button>
+        <p>see the console.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
